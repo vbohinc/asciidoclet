d5cdf3748a0055618fca0215377e44982cacb3b7
Ben Evans <ben@opencloud.com>
2014-07-15 13:03:14 +0100

    Add attributes support

--- src/main/java/org/asciidoctor/asciidoclet/DocletOptions.java
+++ src/main/java/org/asciidoctor/asciidoclet/DocletOptions.java
@@ -1,20 +1,25 @@
 package org.asciidoctor.asciidoclet;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
 import com.sun.javadoc.DocErrorReporter;
 import com.sun.javadoc.RootDoc;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.util.List;
 
 public class DocletOptions {
     public static final String ENCODING = "-encoding";
     public static final String OVERVIEW = "-overview";
     public static final String INCLUDE_BASEDIR = "-include-basedir";
+    public static final String ATTRIBUTES = "-attributes";
 
     private final Optional<File> basedir;
     private final Optional<File> overview;
     private final Charset encoding;
+    private final List<String> attributes;
 
     public static final DocletOptions NONE = new DocletOptions(new String[][]{});
 
@@ -26,6 +31,7 @@ public class DocletOptions {
         File basedir = null;
         File overview = null;
         Charset encoding = Charset.defaultCharset();
+        ImmutableList.Builder<String> attrs = ImmutableList.builder();
         for (String[] option : options) {
             if (option.length > 0) {
                 if (INCLUDE_BASEDIR.equals(option[0])) {
@@ -37,12 +43,16 @@ public class DocletOptions {
                 else if (ENCODING.equals(option[0])) {
                     encoding = Charset.forName(option[1]);
                 }
+                else if (ATTRIBUTES.equals(option[0])) {
+                    attrs.addAll(attributeSplitter.split(option[1]));
+                }
             }
         }
 
         this.basedir = Optional.fromNullable(basedir);
         this.overview = Optional.fromNullable(overview);
         this.encoding = encoding;
+        this.attributes = attrs.build();
     }
 
     public Optional<File> overview() {
@@ -55,6 +65,10 @@ public class DocletOptions {
 
     public Charset encoding() {
         return encoding;
+    }
+
+    public Iterable<String> attributes() {
+        return attributes;
     }
 
     public static boolean validOptions(String[][] options, DocErrorReporter errorReporter, StandardAdapter standardDoclet) {
@@ -71,7 +85,11 @@ public class DocletOptions {
         if (INCLUDE_BASEDIR.equals(option)) {
             return 2;
         }
+        if (ATTRIBUTES.equals(option)) {
+            return 2;
+        }
         return standardDoclet.optionLength(option);
     }
 
+    private static final Splitter attributeSplitter = Splitter.onPattern("\\s*;\\s*").omitEmptyStrings().trimResults();
 }
